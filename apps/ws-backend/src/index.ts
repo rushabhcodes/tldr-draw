7e872a0bca58463c05bd83808faea174c3031398
Rushabh Patil <rushabhpatil.dev@gmail.com>
2025-01-11 21:01:48 +0530

    feat: remove unused docs and add backend applications

---
+++ apps/ws-backend/src/index.ts
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on("connection", (ws) => {
+    ws.on("message", (message) => {
+        console.log("received: %s", message);
+    });
+    
+    ws.send("something");
+    }
+);
